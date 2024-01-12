705f6792c557ca1909082629be5c90bd20a47c8d
Zakaria171 <zakaria.rabih93@gmail.com>
2024-01-12 16:19:39 +0100

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum_uniq = my_list[0]
+    for i in range(1, len(my_list)):
+        sum_uniq = sum_uniq + my_list[i]
+        for j in range(i - 1):
+            if my_list[j] == my_list[i]:
+                sum_uniq = sum_uniq - my_list[i]
+                break
+    return (sum_uniq)
