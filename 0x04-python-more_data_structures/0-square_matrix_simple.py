9714e5260284e7e82b502764c8f41a1aebb604f7
Zakaria171 <zakaria.rabih93@gmail.com>
2024-01-12 01:46:12 +0100

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,5 @@ def square_matrix_simple(matrix=[]):
     sqr_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            sqr_matrix[i][j] = matrix[i][j] **2
+            sqr_matrix[i][j] = matrix[i][j] ** 2
     return (sqr_matrix)
