8e8f5f0da13e3e87c141023dd382715f85f65297
Zakaria171 <zakaria.rabih93@gmail.com>
2024-01-26 20:50:17 +0100

    inventing combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,4 @@ for i in range(0, 10):
         if (i < j) and (i != 8):
             print("{}{},".format(i, j), end=' ')
         elif (i == 8) and (j == 9):
-            print("{}{}".format(i, j), end=' ')
-print('')
+            print("{}{}".format(i, j))
