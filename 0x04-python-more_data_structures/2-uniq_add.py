fc00dc77692cc4d3dac453eacdc668d43d6f740c
Zakaria171 <zakaria.rabih93@gmail.com>
2024-01-27 16:57:27 +0100

    add uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     sum_uniq = my_list[0]
     for i in range(1, len(my_list)):
         sum_uniq = sum_uniq + my_list[i]
-        for j in range(i - 1):
+        for j in range(0, i - 1):
             if my_list[j] == my_list[i]:
                 sum_uniq = sum_uniq - my_list[i]
                 break
